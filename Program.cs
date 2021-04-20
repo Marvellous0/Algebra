7b905f09dda30330d4bf09078a30025aee2fada2
Marvellous0 <81418921+Marvellous0@users.noreply.github.com>
2021-04-20 11:37:22 +0100

    StopWatch

--- Program.cs
+++ Program.cs
@@ -1,50 +1,43 @@
-﻿using System;
+using System;
 
-namespace Algebra
+namespace Assingment
 {
     class Program
     {
-    
-public static void Main()
-{
-    double a, b, c;
-    double disc, deno, x1, x2;
-    Console.WriteLine("ENTER THE VALUES OF A,B,C...");
-    a = Convert.ToDouble(Console.ReadLine());
-    b = Convert.ToDouble(Console.ReadLine());
-    c = Convert.ToDouble(Console.ReadLine());
-    if (a == 0)
-    {
-        x1 = -c / b;
-        Console.WriteLine("The roots are Linear:", x1);
-    }
-    else
-    {
-        disc = (b * b) - (4 * a * c);
-        deno = 2 * a;
-        if (disc > 0)
+
+        // public static void StopWatch()
+        // { 
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private bool _Running;
+
+        public void Start()
         {
-            Console.WriteLine("THE ROOTS ARE REAL AND DISTINCT ROOTS");
-            x1 = (-b / deno) + (Math.Sqrt(disc) / deno);
-            x2 = (-b / deno) - (Math.Sqrt(disc) / deno);
-            Console.WriteLine("THE ROOTS ARE... " + x1 + " and " + x2);
+            if (_Running)
+            {
+                throw new InvalidOperationException("Stopwatch is already running");
+            }
+            _startDate = DateTime.Now;
+            _Running = true;
         }
-        else if (disc == 0)
+
+        public void Stop()
         {
-            Console.WriteLine("THE ROOTS ARE REPEATED ROOTS");
-            x1 = -b / deno;
-            Console.WriteLine("THE ROOT IS...: " + x1);
+            if (!_Running)
+            {
+                throw new InvalidOperationException("Stopwatch is not running");
+            }
+
+
+            _endDate = DateTime.Now;
+            _Running = false;
         }
-        else
+
+        public TimeSpan GetDuration()
         {
-            Console.WriteLine("THE ROOTS ARE IMAGINARY ROOTS\n");
-            x1 = -b / deno;
-            x2 = ((Math.Sqrt((4 * a * c) - (b * b))) / deno);
-            Console.WriteLine("THE ROOT 1: " + x1 + "+i" + x2);
-            Console.WriteLine("THE ROOT 2:" + x1 + "-i" + x2);
+            return _endDate - _startDate;
         }
+
     }
 
-        }
-    }
 }
